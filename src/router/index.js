418cf1de6ac55ec5f789f2a365a8aa0576e388e8
Leandro <leandro.graf@itc-studenten.de>
2024-03-24 01:18:55 +0100

    /firforlife

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import ImpressumView from "@/views/ImpressumView";
 import DatenschutzView from "@/views/DatenschutzView";
 import LoginView from "@/views/LoginView";
@@ -9,77 +9,82 @@ import WebsiteView from "@/views/WebsiteView.vue";
 import WebappView from "@/views/WebappView.vue";
 import EntwicklungView from "@/views/EntwicklungView.vue";
 import ServiceView from "@/views/ServiceView.vue";
+import FitForLifeView from "@/views/FitForLifeView.vue";
 
 const routes = [
 
-  {
-    path: '/impressum',
-    name: 'impressum',
-    component: ImpressumView
-  },  {
-    path: '/service',
-    name: 'Service',
-    component: ServiceView
-  },
-  {
-    path: '/webapp',
-    name: 'Webapp',
-    component: WebappView
-  }, {
-    path: '/entwicklung',
-    name: 'Entwicklung',
-    component: EntwicklungView
-  },
-  {
-    path: '/',
-    name: 'Homeview',
-    component: HomeView
-  },
-  {
-    path: '/website',
-    name: 'Website',
-    component: WebsiteView
-  },
-  {
-    path: '/skills',
-    name: 'SkillsView',
-    component: SkillsView
-  },
-  {
-    path: '/datenschutz',
-    name: 'datenschutz',
-    component: DatenschutzView
-  },{
-    path: '/rechnung',
-    name: 'rechnung',
-    component: RechnungenView
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginView
-  },
+    {
+        path: '/impressum',
+        name: 'impressum',
+        component: ImpressumView
+    }, {
+        path: '/service',
+        name: 'Service',
+        component: ServiceView
+    },
+    {
+        path: '/webapp',
+        name: 'Webapp',
+        component: WebappView
+    }, {
+        path: '/entwicklung',
+        name: 'Entwicklung',
+        component: EntwicklungView
+    },
+    {
+        path: '/',
+        name: 'Homeview',
+        component: HomeView
+    },
+    {
+        path: '/website',
+        name: 'Website',
+        component: WebsiteView
+    },
+    {
+        path: '/skills',
+        name: 'SkillsView',
+        component: SkillsView
+    },
+    {
+        path: '/datenschutz',
+        name: 'datenschutz',
+        component: DatenschutzView
+    }, {
+        path: '/rechnung',
+        name: 'rechnung',
+        component: RechnungenView
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginView
+    },
+    {
+        path: '/fitforlife',
+        name: 'firforlife',
+        component: FitForLifeView
+    },
 
 
-
-  {
-    path: '/:catchAll(.*)', component: HomeView
-  },
+    {
+        path: '/:catchAll(.*)', component: HomeView
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-  scrollBehavior: function (to) {
-    if (to.hash) {
-      return {
-        el: to.hash,
-        behavior: 'smooth',
-      }
-    } else {
-      return {el: document.getElementById('home'), behavior: 'smooth'}
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+    scrollBehavior: function (to) {
+        if (to.hash) {
+            return {
+                el: to.hash,
+                behavior: 'smooth',
+            }
+        } else {
+            return {el: document.getElementById('home'), behavior: 'smooth'}
+        }
     }
-  }
 })
 
 export default router
---
+++ src/views/FitForLifeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div
+      style="position: fixed; top: 0; z-index: 100; background-color: #ffffff;height: 100vh; width: 100vw">
+    <iframe src="https://fastglobeit.de:8093" style="width: 100vw; height: 100vh"></iframe>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
